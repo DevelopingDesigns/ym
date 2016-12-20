ae63b44642678912b09703457d5fef722c9d297e
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-19 22:02:37 -0500

    Add Resource Types

--- wp-content/mu-plugins/resources/resources.php
+++ wp-content/mu-plugins/resources/resources.php
@@ -2,7 +2,7 @@
 
 add_action( 'init', 'resources_create_post_type', 0 );
 /**
- * Register custom post type
+ * Register Custom Post Type
  */
 function resources_create_post_type() {
 
@@ -73,4 +73,53 @@ function resources_create_post_type() {
 	register_post_type( 'resource', $args );
 
 }
+
+add_action( 'init', 'resources_create_taxonomy', 0 );
+/**
+ * Register Custom Taxonomy
+ */
+function resources_create_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Types', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Taxonomy', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Item', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                       => 'resource-type',
+		'with_front'                 => true,
+		'hierarchical'               => false,
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => $rewrite,
+		'show_in_rest'               => true,
+	);
+	register_taxonomy( 'resource_type', array( 'resource' ), $args );
+
+}
+
 
