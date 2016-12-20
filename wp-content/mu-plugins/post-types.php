8aad3d24f3e435e097e38c6b384915b1284fd512
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-19 21:24:04 -0500

    Add custom post types support

---
+++ wp-content/mu-plugins/post-types.php
@@ -0,0 +1,13 @@
+<?php
+
+define( 'DD_MU_TEXT_DOMAIN', 'dd-mu-post-types' );
+
+require_once('articles/articles.php');
+require_once('infographics/infographics.php');
+require_once('landing-pages/landing-pages.php');
+require_once('logins/logins.php');
+require_once('partners/partners.php');
+require_once('resources/resources.php');
+require_once('team/team.php');
+require_once('videos/videos.php');
+require_once('webinars/webinars.php');
