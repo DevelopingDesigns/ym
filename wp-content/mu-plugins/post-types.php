52c4184c4ba56b3296150dc75cc4304fd8a4fa04
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-27 16:41:35 -0500

    Remove resources for now

--- wp-content/mu-plugins/post-types.php
+++ wp-content/mu-plugins/post-types.php
@@ -37,7 +37,7 @@ require_once('landing-pages/landing-pages.php');
 require_once('locations/locations.php');
 require_once('logins/logins.php');
 require_once('partners/partners.php');
-require_once('resources/resources.php');
+//require_once('resources/resources.php');
 require_once('videos/videos.php');
 require_once('webinars/webinars.php');
 
