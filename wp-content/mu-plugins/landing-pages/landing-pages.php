e228ae5597ef07a8b7cf9b22cfae71b14aaefa66
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-27 13:04:27 -0500

    Make public and add schema

--- wp-content/mu-plugins/landing-pages/landing-pages.php
+++ wp-content/mu-plugins/landing-pages/landing-pages.php
@@ -51,7 +51,8 @@ function landing_pages_create_post_type() {
 			'excerpt',
 			'revisions',
 			'thumbnail',
-			'custom-fields',
+//			'custom-fields',
+			'page-attributes',
 		),
 		'hierarchical'        => true,
 		'public'              => true,
@@ -63,13 +64,181 @@ function landing_pages_create_post_type() {
 		'show_in_nav_menus'   => true,
 		'can_export'          => true,
 		'has_archive'         => false,
-		'exclude_from_search' => true,
-		'publicly_queryable'  => false,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
 		'rewrite'             => $rewrite,
 		'capability_type'     => 'page',
 		'show_in_rest'        => true,
 	);
 	register_post_type( 'landing-page', $args );
 
+	new WPS_Schema( 'landing-page', 'article' );
 }
 
+add_action( 'acf/init', 'landing_pages_create_acf' );
+/**
+ * Creates ACF Fields for post type
+ */
+function landing_pages_create_acf() {
+	if ( function_exists( 'acf_add_local_field_group' ) ) {
+//wp_die('test');
+		acf_add_local_field_group( array(
+			'id'         => 'lpcontent',
+			'key'      => 'landing_page',
+			'title'      => __( 'Landing Pages Content', DD_MU_TEXT_DOMAIN ),
+			'fields'     => array(
+				array(
+					'key'          => 'pc0',
+					'label'        => __( 'Landing Pages Content', DD_MU_TEXT_DOMAIN ),
+					'name'         => 'program_content',
+					'type'         => 'flexible_content',
+					'layouts'      => array(
+						array(
+							'label'      => 'Intro',
+							'name'       => 'intro',
+							'display'    => 'row',
+							'min'        => '',
+							'max'        => '',
+							'sub_fields' => array(
+								array(
+									'key'           => 'pc1',
+									'label'         => 'Heading',
+									'name'          => 'heading',
+									'type'          => 'text',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'prepend'       => '',
+									'append'        => '',
+									'formatting'    => 'html',
+									'maxlength'     => '',
+								),
+								array(
+									'key'           => 'pc2',
+									'label'         => 'Intro Text',
+									'name'          => 'intro_text',
+									'type'          => 'textarea',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'maxlength'     => '',
+									'rows'          => '',
+									'formatting'    => 'br',
+								),
+							),
+						),
+						array(
+							'label'      => 'Inline Image',
+							'name'       => 'inline_image',
+							'display'    => 'row',
+							'min'        => '',
+							'max'        => '',
+							'sub_fields' => array(
+								array(
+									'key'          => 'pc3',
+									'label'        => 'Image',
+									'name'         => 'image',
+									'type'         => 'image',
+									'column_width' => '',
+									'save_format'  => 'object',
+									'preview_size' => 'full',
+									'library'      => 'all',
+								),
+								array(
+									'key'           => 'pc4',
+									'label'         => 'Caption',
+									'name'          => 'caption',
+									'type'          => 'textarea',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'maxlength'     => '',
+									'rows'          => '',
+									'formatting'    => 'br',
+								),
+							),
+						),
+						array(
+							'label'      => 'CTA block',
+							'name'       => 'cta_block',
+							'display'    => 'row',
+							'min'        => '',
+							'max'        => '',
+							'sub_fields' => array(
+								array(
+									'key'           => 'pc5',
+									'label'         => 'Block heading',
+									'name'          => 'block_heading',
+									'type'          => 'text',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'prepend'       => '',
+									'append'        => '',
+									'formatting'    => 'html',
+									'maxlength'     => '',
+								),
+								array(
+									'key'           => 'pc6',
+									'label'         => 'Block Text',
+									'name'          => 'block_text',
+									'type'          => 'textarea',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'maxlength'     => '',
+									'rows'          => '',
+									'formatting'    => 'br',
+								),
+								array(
+									'key'           => 'pc7',
+									'label'         => 'Block Button',
+									'name'          => 'block_button',
+									'type'          => 'text',
+									'column_width'  => '',
+									'default_value' => '',
+									'placeholder'   => '',
+									'prepend'       => '',
+									'append'        => '',
+									'formatting'    => 'html',
+									'maxlength'     => '',
+								),
+							),
+						),
+					),
+//					'button_label' => __( 'Add Row', DD_MU_TEXT_DOMAIN ),
+//					'min'          => '',
+//					'max'          => '',
+				),
+			),
+			'location'   => array(
+				array(
+					array(
+						'param'    => 'post_type',
+						'operator' => '==',
+						'value'    => 'landing-page',
+//						'order_no' => 0,
+//						'group_no' => 0,
+					),
+				),
+			),
+//			'options'    => array(
+//				'position'       => 'normal',
+//				'layout'         => 'no_box',
+//				'hide_on_screen' => array(),
+//			),
+//			'menu_order' => 0,
+		) );
+	}
+}
+
+//add_action( 'plugins_loaded', 'resources_acf_content_blocks_support', 15 );
+function resources_acf_content_blocks_support() {
+	if ( function_exists( 'ym_add_flexible_content_support' ) ) {
+		ym_add_flexible_content_support( array (
+			'param' => 'post_type',
+			'operator' => '==',
+			'value' => 'landing-page',
+		) );
+	}
+}
