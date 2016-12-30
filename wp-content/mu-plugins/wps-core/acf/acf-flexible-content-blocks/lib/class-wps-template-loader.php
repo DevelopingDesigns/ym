0276b7c8a24bbd6dcfce23509c8e3454611e475a
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-29 21:40:12 -0500

    Replace template loader

--- wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/lib/class-wps-template-loader.php
+++ wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/lib/class-wps-template-loader.php
@@ -2,46 +2,50 @@
 /**
  * Template Loader for Plugins.
  *
- * @package   Gamajo_Template_Loader
- * @author    Gary Jones
- * @link      http://github.com/GaryJones/Gamajo-Template-Loader
- * @copyright 2013 Gary Jones
+ * @package   WPS_Template_Loader
+ * @author    Travis Smith <t@wpsmith.net>
+ * @copyright 2014-2017 Travis Smith
  * @license   GPL-2.0+
  * @version   1.2.0
  */
 
-if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	/**
-	 * Template loader.
-	 *
-	 * Originally based on functions in Easy Digital Downloads (thanks Pippin!).
-	 *
-	 * When using in a plugin, create a new class that extends this one and just overrides the properties.
-	 *
-	 * @package Gamajo_Template_Loader
-	 * @author  Gary Jones
-	 */
-	class Gamajo_Template_Loader {
+/**
+ * Template loader.
+ *
+ * Based on Gamajo's Gamajo_Template_Loader.
+ * Originally based on functions in Easy Digital Downloads (thanks Pippin!).
+ *
+ * When using in a plugin, create a new class that extends this one and just overrides the properties.
+ *
+ * @package WPS_Core
+ * @author  Travis Smith <t@wpsmith.net>
+ */
+if ( !class_exists( 'WPS_Template_Loader' ) ) {
+	class WPS_Template_Loader extends WPS_Singleton {
+
 		/**
-		 * Prefix for filter names.
+		 * Prefix for filter names. _ will be added at the end.
 		 *
+		 * e.g., 'your_plugin'
 		 * @since 1.0.0
-		 *
-		 * @var string
+		 * @type string
 		 */
-		protected $filter_prefix = 'your_plugin';
+		protected $filter_prefix = 'yourplugin'; // or
 
 		/**
 		 * Directory name where custom templates for this plugin should be found in the theme.
 		 *
-		 * For example: 'your-plugin-templates'.
+		 * e.g., 'your-plugin-templates'.
 		 *
 		 * @since 1.0.0
-		 *
-		 * @var string
+		 * @type string
 		 */
-		protected $theme_template_directory = 'plugin-templates';
+		protected $theme_template_directory = 'templates'; // or 'your-plugin' or 'your-plugin-templates' etc.
 
 		/**
 		 * Reference to the root directory path of this plugin.
@@ -51,23 +55,65 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 		 * e.g. YOUR_PLUGIN_TEMPLATE or plugin_dir_path( dirname( __FILE__ ) ); etc.
 		 *
 		 * @since 1.0.0
-		 *
-		 * @var string
+		 * @type string
 		 */
 		protected $plugin_directory = 'YOUR_PLUGIN_DIR';
 
 		/**
-		 * Directory name where templates are found in this plugin.
+		 * Reference to the template directory path of this plugin.
 		 *
 		 * Can either be a defined constant, or a relative reference from where the subclass lives.
 		 *
 		 * e.g. 'templates' or 'includes/templates', etc.
 		 *
-		 * @since 1.1.0
-		 *
-		 * @var string
+		 * @since 1.0.0
+		 * @type string
 		 */
-		protected $plugin_template_directory = 'templates';
+		protected $templates_directory = 'templates';
+
+		/**
+		 * Constructor to enforce abstracted properties.
+		 *
+		 * @since 1.1.0
+		 * @uses  WPS_Enforcer Enforces abstracted properties.
+		 */
+		protected function __construct( $args = array() ) {
+
+			if ( ! empty( $args ) ) {
+
+				$defaults = array(
+					'filter_prefix'            => 'yourplugin',
+					// or 'your_plugin'
+					'theme_template_directory' => 'templates',
+					// or 'your-plugin' or 'your-plugin-templates' etc.
+					'plugin_directory'         => 'yourplugin',
+					// or YOUR_PLUGIN_DIR or plugin_dir_path( dirname( __FILE__ ) ); etc.
+					'templates_directory'      => 'templates',
+					// or includes/templates, etc.
+				);
+
+				$args = wp_parse_args( $args, $defaults );
+
+				foreach ( $args as $var => $val ) {
+					$this->{$var} = $val;
+				}
+
+			}
+
+			$this->init( $this );
+
+		}
+
+		public function init( $obj ) {
+
+			if ( method_exists( $obj, 'loaded' ) ) {
+				if ( ! did_action( 'after_setup_theme' ) ) {
+					add_action( 'after_setup_theme', array( $obj, 'loaded' ), 99 );
+				} else {
+					$this->loaded();
+				}
+			}
+		}
 
 		/**
 		 * Clean up template data.
@@ -76,48 +122,6 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 		 */
 		public function __destruct() {
 			$this->unset_template_data();
-		}
-
-		/**
-		 * Retrieve a template part.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param string $slug Template slug.
-		 * @param string $name Optional. Template variation name. Default null.
-		 * @param bool $load Optional. Whether to load template. Default true.
-		 *
-		 * @return string
-		 */
-		public function get_template_part( $slug, $name = null, $load = true ) {
-			// Execute code for this part.
-			do_action( 'get_template_part_' . $slug, $slug, $name );
-
-			// Get files names of templates, for given slug and name.
-			$templates = $this->get_template_file_names( $slug, $name );
-
-			// Return the part that is found.
-			return $this->locate_template( $templates, $load, false );
-		}
-
-		/**
-		 * Make custom data available to template.
-		 *
-		 * Data is available to the template as properties under the `$data` variable.
-		 * i.e. A value provided here under `$data['foo']` is available as `$data->foo`.
-		 *
-		 * When an input key has a hyphen, you can use `$data->{foo-bar}` in the template.
-		 *
-		 * @since 1.2.0
-		 *
-		 * @param array $data Custom data for the template.
-		 * @param string $var_name Optional. Variable under which the custom data is available in the template.
-		 *                         Default is 'data'.
-		 */
-		public function set_template_data( array $data, $var_name = 'data' ) {
-			global $wp_query;
-
-			$wp_query->query_vars[ $var_name ] = (object) $data;
 		}
 
 		/**
@@ -136,12 +140,38 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 		}
 
 		/**
+		 * Retrieve a template part.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @uses  Gamajo_Template_Loader::get_template_possble_parts() Create file names of templates.
+		 * @uses  Gamajo_Template_Loader::locate_template() Retrieve the name of the highest priority template
+		 *     file that exists.
+		 *
+		 * @param string $slug
+		 * @param string $name Optional. Default null.
+		 * @param bool   $load Optional. Default false.
+		 *
+		 * @return string
+		 */
+		public function get_template_part( $slug, $name = null, $load = false ) {
+			// Execute code for this part
+			do_action( 'get_template_part_' . $slug, $slug, $name );
+
+			// Get files names of templates, for given slug and name.
+			$templates = $this->get_template_file_names( $slug, $name );
+
+			// Return the part that is found
+			return $this->locate_template( $templates, $load, false );
+		}
+
+		/**
 		 * Given a slug and optional name, create the file names of templates.
 		 *
 		 * @since 1.0.0
 		 *
-		 * @param string $slug Template slug.
-		 * @param string $name Template variation name.
+		 * @param string $slug
+		 * @param string $name
 		 *
 		 * @return array
 		 */
@@ -150,6 +180,7 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 			if ( isset( $name ) ) {
 				$templates[] = $slug . '-' . $name . '.php';
 			}
+			$templates[] = $name . '.php';
 			$templates[] = $slug . '.php';
 
 			/**
@@ -160,10 +191,11 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 			 *
 			 * @since 1.0.0
 			 *
-			 * @param array $templates Names of template files that should be looked for, for given slug and name.
-			 * @param string $slug Template slug.
-			 * @param string $name Template variation name.
+			 * @param array  $templates Names of template files that should be looked for, for given slug and name.
+			 * @param string $slug      Template slug.
+			 * @param string $name      Template name.
 			 */
+
 			return apply_filters( $this->filter_prefix . '_get_template_part', $templates, $slug, $name );
 		}
 
@@ -176,27 +208,29 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 		 *
 		 * @since 1.0.0
 		 *
+		 * @uses  Gamajo_Tech_Loader::get_template_paths() Return a list of paths to check for template locations.
+		 *
 		 * @param string|array $template_names Template file(s) to search for, in order.
-		 * @param bool $load If true the template file will be loaded if it is found.
-		 * @param bool $require_once Whether to require_once or require. Default true.
+		 * @param bool         $load           If true the template file will be loaded if it is found.
+		 * @param bool         $require_once   Whether to require_once or require. Default true.
 		 *                                     Has no effect if $load is false.
 		 *
 		 * @return string The template filename if one is located.
 		 */
 		public function locate_template( $template_names, $load = false, $require_once = true ) {
-			// No file found yet.
+			// No file found yet
 			$located = false;
 
-			// Remove empty entries.
+			// Remove empty entries
 			$template_names = array_filter( (array) $template_names );
 			$template_paths = $this->get_template_paths();
 
-			// Try to find a template file.
+			// Try to find a template file
 			foreach ( $template_names as $template_name ) {
-				// Trim off any slashes from the template name.
-				$template_name = ltrim( $template_name, '/' );
+				// Trim off any slashes from the template name
+				$template_name = str_replace( ' ', '-', ltrim( $template_name, '/' ) );
 
-				// Try locating this template file by looping through the template paths.
+				// Try locating this template file by looping through the template paths
 				foreach ( $template_paths as $template_path ) {
 					if ( file_exists( $template_path . $template_name ) ) {
 						$located = $template_path . $template_name;
@@ -228,7 +262,7 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 
 			$file_paths = array(
 				10  => trailingslashit( get_template_directory() ) . $theme_directory,
-				100 => $this->get_templates_dir(),
+				100 => $this->get_templates_dir()
 			);
 
 			// Only add this conditionally, so non-child themes don't redundantly check active theme twice.
@@ -245,7 +279,7 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 			 */
 			$file_paths = apply_filters( $this->filter_prefix . '_template_paths', $file_paths );
 
-			// Sort the file paths based on priority.
+			// sort the file paths based on priority
 			ksort( $file_paths, SORT_NUMERIC );
 
 			return array_map( 'trailingslashit', $file_paths );
@@ -261,7 +295,9 @@ if ( ! class_exists( 'Gamajo_Template_Loader' ) ) {
 		 * @return string
 		 */
 		protected function get_templates_dir() {
-			return trailingslashit( $this->plugin_directory ) . $this->plugin_template_directory;
+
+			return trailingslashit( $this->plugin_directory ) . $this->templates_directory;
 		}
+
 	}
 }
