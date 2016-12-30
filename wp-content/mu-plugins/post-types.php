bcd24211ff11b0705c1695cbfe6a51292b65c5a5
Travis Smith <travis.smith@Traviss-MBP.lan>
2016-12-30 15:36:57 -0500

    Add services cptonomy

--- wp-content/mu-plugins/post-types.php
+++ wp-content/mu-plugins/post-types.php
@@ -40,6 +40,7 @@ require_once('logins/logins.php');
 require_once('partner-programs/partner-programs.php');
 require_once('products/products.php');
 //require_once('resources/resources.php');
+require_once('services/services.php');
 require_once('testimonials/testimonials.php');
 require_once('videos/videos.php');
 require_once('webinars/webinars.php');
