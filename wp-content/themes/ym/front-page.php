345329962ff8d0340896edd45924adf85cd0823e
Joe Dooley <hello@developingdesigns.com>
2017-01-03 13:09:52 -0500

    updated headings with correct font-sizes

--- wp-content/themes/ym/front-page.php
+++ wp-content/themes/ym/front-page.php
@@ -11,7 +11,15 @@
  * @version 1.0
  */
 
+/**
+ * Remove entry header
+ */
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+/**
+ * Force full width layout
+ */
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 
 genesis();
