6df766333639ed1b7f35ccf61992443c182b0412
Travis Smith <t@wpsmith.net>
2017-01-03 20:33:40 -0500

    Fix templates

---
+++ wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/templates/blocks/layout-hero.php
@@ -0,0 +1,3 @@
+<?php
+fcb_template( 'blocks/parts/block-media', get_row_layout(), 1 );
+fcb_template( 'blocks/parts/block-cta', get_row_layout(), 1 );
---
+++ wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/templates/blocks/parts/block-subtitle.php
@@ -0,0 +1,7 @@
+<?php if ( get_sub_field( 'subtitle' ) ): ?>
+	<header class="block-subtitle">
+		<div class="block-subtitle-inner">
+			<?php the_block_title( 'subtitle' ); ?>
+		</div>
+	</header>
+<?php endif; ?>
