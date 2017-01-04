ffd20d2a80faff9f210e312061fcf9614c37a990
Travis Smith <t@wpsmith.net>
2017-01-03 21:57:36 -0500

    Refactor with defaults flexible content methods

--- wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/lib/class-acffcb-flexible-content.php
+++ wp-content/mu-plugins/wps-core/acf/acf-flexible-content-blocks/lib/class-acffcb-flexible-content.php
@@ -43,25 +43,14 @@ class FlexibleContent {
 	 *
 	 * Flexible Content field for Calls to Action
 	 */
-	public function cta( $thisKey = 'flexible' ) {
+	public function cta( $thisKey = 'flexible', $args = array() ) {
 		$FCBFlexibleContentFields = new Fields( $this->layout, __FUNCTION__ );
 
-		return (
-		array(
+		return wp_parse_args( (array) $args, $this->defaults( array(
 			'key'               => $this->key . $thisKey . '-' . $this->getCallingFunctionName() . __FUNCTION__,
 			'label'             => __( 'Calls to Action', ACFFCB_PLUGIN_DOMAIN ),
 			'name'              => 'calls_to_action',
-			'type'              => 'flexible_content',
-			'instructions'      => '',
-			'required'          => 0,
-			'conditional_logic' => 0,
-			'wrapper'           => array(
-				'width' => '',
-				'class' => 'acf-cta',
-				'id'    => '',
-			),
-			'min'               => '',
-			'max'               => '',
+			'wrapper'      => fcb_get_wrapper( null, 'acf-cta' ),
 			'button_label'      => __( 'Add Call to Action', ACFFCB_PLUGIN_DOMAIN ),
 			'layouts'           => array(
 				array(
@@ -94,8 +83,7 @@ class FlexibleContent {
 				),
 
 			)
-		)
-		);
+		)));
 	}
 
 	/**
@@ -107,27 +95,17 @@ class FlexibleContent {
 	 *
 	 * Flexible Content field for Locations
 	 */
-	public function location( $thisKey = 'flexible' ) {
+	public function location( $thisKey = 'flexible', $args = array() ) {
 		$FCBFlexibleContentFields = new Fields( $this->layout, __FUNCTION__ );
 
-		return (
-		array(
-			'key'               => $this->key . $thisKey . '-' . $this->getCallingFunctionName() . __FUNCTION__,
-			'label'             => __( 'Location', ACFFCB_PLUGIN_DOMAIN ),
-			'name'              => 'location',
-			'type'              => 'flexible_content',
-			'instructions'      => '',
-			'required'          => 0,
-			'conditional_logic' => 0,
-			'wrapper'           => array(
-				'width' => '',
-				'class' => 'acf-location',
-				'id'    => '',
-			),
-			'min'               => '',
-			'max'               => 1,
-			'button_label'      => __( 'Add Location', ACFFCB_PLUGIN_DOMAIN ),
-			'layouts'           => array(
+		return wp_parse_args( (array) $args, $this->defaults( array(
+			'key'          => $this->key . $thisKey . '-' . $this->getCallingFunctionName() . __FUNCTION__,
+			'label'        => __( 'Location', ACFFCB_PLUGIN_DOMAIN ),
+			'name'         => 'location',
+			'wrapper'      => fcb_get_wrapper( null, 'acf-location' ),
+			'max'          => 1,
+			'button_label' => __( 'Add Location', ACFFCB_PLUGIN_DOMAIN ),
+			'layouts'      => array(
 				array(
 					'key'          => $this->key . $this->getCallingFunctionName() . __FUNCTION__ . '-location',
 					'name'         => 'external_address',
@@ -179,8 +157,7 @@ class FlexibleContent {
 					),
 				),
 			)
-		)
-		);
+		) ) );
 	}
 
 	/**
@@ -192,7 +169,7 @@ class FlexibleContent {
 	 *
 	 * Flexible Content field for Calls to Action
 	 */
-	public function media( $min = 0, $max = 1, $thisKey = 'flexible' ) {
+	public function media( $min = 0, $max = 1, $thisKey = 'flexible', $args = array() ) {
 		$FCBFlexibleContentFields = new Fields( $this->layout, __FUNCTION__ );
 
 		$icon_fonts      = fcb_get_icon_fonts();
@@ -227,23 +204,15 @@ class FlexibleContent {
 		) );
 		$icon_sub_fields[] = $FCBFlexibleContentFields->icon_preview( $thisKey );
 
-		return array(
-			'key'               => $this->key . $thisKey . '-' . $this->getCallingFunctionName() . __FUNCTION__,
-			'label'             => __( 'Media', ACFFCB_PLUGIN_DOMAIN ),
-			'name'              => 'media',
-			'type'              => 'flexible_content',
-			'instructions'      => '',
-			'required'          => 0,
-			'conditional_logic' => 0,
-			'wrapper'           => array(
-				'width' => '',
-				'class' => 'acf-media',
-				'id'    => '',
-			),
-			'min'               => $min,
-			'max'               => $max,
-			'button_label'      => __( 'Add Media', ACFFCB_PLUGIN_DOMAIN ),
-			'layouts'           => array(
+		return wp_parse_args( (array) $args, $this->defaults( array(
+			'key'          => $this->key . $thisKey . '-' . $this->getCallingFunctionName() . __FUNCTION__,
+			'label'        => __( 'Media', ACFFCB_PLUGIN_DOMAIN ),
+			'name'         => 'media',
+			'wrapper'      => fcb_get_wrapper( null, 'acf-media' ),
+			'min'          => $min,
+			'max'          => $max,
+			'button_label' => __( 'Add Media', ACFFCB_PLUGIN_DOMAIN ),
+			'layouts'      => array(
 				array(
 					'key'        => $this->key . $this->getCallingFunctionName() . __FUNCTION__ . '-image',
 					'name'       => 'image',
@@ -331,251 +300,25 @@ class FlexibleContent {
 					),
 				),
 			)
-		);
+		) ) );
+	}
+
+	protected function defaults( $args ) {
+		return apply_filters( 'fcb_flexible_content_' . $args['type'] . '_defaults', wp_parse_args( (array) $args, array(
+			'type'              => 'flexible_content',
+			'instructions'      => '',
+			'message'           => '',
+			'required'          => 0,
+			'conditional_logic' => 0,
+			'wrapper'           => fcb_get_wrapper( null, 'acf-location' ),
+			'min'               => '',
+			'max'               => '',
+			'layouts'           => array(),
+		) ), $args );
 	}
 
 	private function get_countries() {
-		return array(
-			"us" => "United States",
-			"af" => "Afghanistan",
-			"al" => "Albania",
-			"dz" => "Algeria",
-			"as" => "American Samoa",
-			"ad" => "Andorra",
-			"ad" => "Angola",
-			"ai" => "Anguilla",
-			"aq" => "Antarctica",
-			"ag" => "Antigua and Barbuda",
-			"ar" => "Argentina",
-			"am" => "Armenia",
-			"aw" => "Aruba",
-			"au" => "Australia",
-			"at" => "Austria",
-			"az" => "Azerbaijan",
-			"bs" => "Bahamas",
-			"bh" => "Bahrain",
-			"bd" => "Bangladesh",
-			"bb" => "Barbados",
-			"by" => "Belarus",
-			"be" => "Belgium",
-			"bz" => "Belize",
-			"bj" => "Benin",
-			"bm" => "Bermuda",
-			"bt" => "Bhutan",
-			"bo" => "Bolivia",
-			"ba" => "Bosnia and Herzegowina",
-			"bw" => "Botswana",
-			"bv" => "Bouvet Island",
-			"br" => "Brazil",
-			"io" => "British Indian Ocean Territory",
-			"bn" => "Brunei Darussalam",
-			"bg" => "Bulgaria",
-			"bf" => "Burkina Faso",
-			"bi" => "Burundi",
-			"kh" => "Cambodia",
-			"cm" => "Cameroon",
-			"ca" => "Canada",
-			"cv" => "Cabo Verde",
-			"ky" => "Cayman Islands",
-			"cf" => "Central African Republic",
-			"td" => "Chad",
-			"cl" => "Chile",
-			"cn" => "China",
-			"cx" => "Christmas Island",
-			"cc" => "Cocos (Keeling) Islands",
-			"co" => "Colombia",
-			"km" => "Comoros",
-			"cg" => "Congo",
-			"cd" => "Congo, the Democratic Republic of the",
-			"ck" => "Cook Islands",
-			"cr" => "Costa Rica",
-			"ci" => "Cote d'Ivoire",
-			"hr" => "Croatia (Hrvatska)",
-			"cu" => "Cuba",
-			"cy" => "Cyprus",
-			"cz" => "Czech Republic",
-			"dk" => "Denmark",
-			"dj" => "Djibouti",
-			"dm" => "Dominica",
-			"do" => "Dominican Republic",
-			"tl" => "East Timor",
-			"ec" => "Ecuador",
-			"eg" => "Egypt",
-			"sv" => "El Salvador",
-			"gq" => "Equatorial Guinea",
-			"er" => "Eritrea",
-			"ee" => "Estonia",
-			"et" => "Ethiopia",
-			"fk" => "Falkland Islands (Malvinas)",
-			"fo" => "Faroe Islands",
-			"fj" => "Fiji",
-			"fi" => "Finland",
-			"fr" => "France",
-			"gf" => "French Guiana",
-			"pf" => "French Polynesia",
-			"tf" => "French Southern Territories",
-			"ga" => "Gabon",
-			"gm" => "Gambia",
-			"ge" => "Georgia",
-			"de" => "Germany",
-			"gh" => "Ghana",
-			"gi" => "Gibraltar",
-			"gr" => "Greece",
-			"gl" => "Greenland",
-			"gd" => "Grenada",
-			"gp" => "Guadeloupe",
-			"gu" => "Guam",
-			"gt" => "Guatemala",
-			"gn" => "Guinea",
-			"gw" => "Guinea-Bissau",
-			"gy" => "Guyana",
-			"ht" => "Haiti",
-			"hm" => "Heard and Mc Donald Islands",
-			"va" => "Holy See (Vatican City State)",
-			"hn" => "Honduras",
-			"hk" => "Hong Kong",
-			"hu" => "Hungary",
-			"is" => "Iceland",
-			"in" => "India",
-			"id" => "Indonesia",
-			"ir" => "Iran (Islamic Republic of)",
-			"iq" => "Iraq",
-			"ie" => "Ireland",
-			"il" => "Israel",
-			"it" => "Italy",
-			"jm" => "Jamaica",
-			"jp" => "Japan",
-			"jo" => "Jordan",
-			"kz" => "Kazakhstan",
-			"ke" => "Kenya",
-			"ki" => "Kiribati",
-			"kp" => "Korea, Democratic People's Republic of",
-			"kr" => "Korea, Republic of",
-			"kw" => "Kuwait",
-			"kg" => "Kyrgyzstan",
-			"la" => "Lao, People's Democratic Republic",
-			"lv" => "Latvia",
-			"lb" => "Lebanon",
-			"ls" => "Lesotho",
-			"lr" => "Liberia",
-			"ly" => "Libyan Arab Jamahiriya",
-			"li" => "Liechtenstein",
-			"lt" => "Lithuania",
-			"lu" => "Luxembourg",
-			"mo" => "Macao",
-			"mk" => "Macedonia, The Former Yugoslav Republic of",
-			"mg" => "Madagascar",
-			"mw" => "Malawi",
-			"my" => "Malaysia",
-			"mv" => "Maldives",
-			"ml" => "Mali",
-			"mt" => "Malta",
-			"mh" => "Marshall Islands",
-			"mq" => "Martinique",
-			"mr" => "Mauritania",
-			"mu" => "Mauritius",
-			"yt" => "Mayotte",
-			"mx" => "Mexico",
-			"fm" => "Micronesia, Federated States of",
-			"md" => "Moldova, Republic of",
-			"mc" => "Monaco",
-			"mn" => "Mongolia",
-			"ms" => "Montserrat",
-			"ma" => "Morocco",
-			"mz" => "Mozambique",
-			"mm" => "Myanmar",
-			"na" => "Namibia",
-			"nr" => "Nauru",
-			"np" => "Nepal",
-			"nl" => "Netherlands",
-			"an" => "Netherlands Antilles",
-			"nc" => "New Caledonia",
-			"nz" => "New Zealand",
-			"ni" => "Nicaragua",
-			"ne" => "Niger",
-			"ng" => "Nigeria",
-			"nu" => "Niue",
-			"nf" => "Norfolk Island",
-			"mp" => "Northern Mariana Islands",
-			"no" => "Norway",
-			"om" => "Oman",
-			"pk" => "Pakistan",
-			"pw" => "Palau",
-			"pa" => "Panama",
-			"pg" => "Papua New Guinea",
-			"py" => "Paraguay",
-			"pe" => "Peru",
-			"ph" => "Philippines",
-			"pn" => "Pitcairn",
-			"pl" => "Poland",
-			"pt" => "Portugal",
-			"pr" => "Puerto Rico",
-			"qa" => "Qatar",
-			"re" => "Reunion",
-			"ro" => "Romania",
-			"ru" => "Russian Federation",
-			"rw" => "Rwanda",
-			"kn" => "Saint Kitts and Nevis",
-			"lc" => "Saint Lucia",
-			"vc" => "Saint Vincent and the Grenadines",
-			"ws" => "Samoa",
-			"sm" => "San Marino",
-			"st" => "Sao Tome and Principe",
-			"sa" => "Saudi Arabia",
-			"sn" => "Senegal",
-			"sc" => "Seychelles",
-			"sl" => "Sierra Leone",
-			"sg" => "Singapore",
-			"sk" => "Slovakia (Slovak Republic)",
-			"si" => "Slovenia",
-			"sb" => "Solomon Islands",
-			"so" => "Somalia",
-			"za" => "South Africa",
-			"gs" => "South Georgia and the South Sandwich Islands",
-			"es" => "Spain",
-			"lk" => "Sri Lanka",
-			"sh" => "St. Helena",
-			"pm" => "St. Pierre and Miquelon",
-			"sd" => "Sudan",
-			"sr" => "Suriname",
-			"sj" => "Svalbard and Jan Mayen Islands",
-			"sz" => "Swaziland",
-			"se" => "Sweden",
-			"ch" => "Switzerland",
-			"sy" => "Syrian Arab Republic",
-			"tw" => "Taiwan, Province of China",
-			"tj" => "Tajikistan",
-			"tz" => "Tanzania, United Republic of",
-			"th" => "Thailand",
-			"tg" => "Togo",
-			"tk" => "Tokelau",
-			"to" => "Tonga",
-			"tt" => "Trinidad and Tobago",
-			"tn" => "Tunisia",
-			"tr" => "Turkey",
-			"tm" => "Turkmenistan",
-			"tc" => "Turks and Caicos Islands",
-			"tv" => "Tuvalu",
-			"ug" => "Uganda",
-			"ua" => "Ukraine",
-			"ae" => "United Arab Emirates",
-			"gb" => "United Kingdom",
-			"us" => "United States",
-			"um" => "United States Minor Outlying Islands",
-			"uy" => "Uruguay",
-			"uz" => "Uzbekistan",
-			"vu" => "Vanuatu",
-			"ve" => "Venezuela",
-			"vn" => "Vietnam",
-			"vg" => "Virgin Islands (British)",
-			"vi" => "Virgin Islands (U.S.)",
-			"wf" => "Wallis and Futuna Islands",
-			"eh" => "Western Sahara",
-			"ye" => "Yemen",
-			"yu" => "Serbia",
-			"zm" => "Zambia",
-			"zw" => "Zimbabwe",
-		);
+		return fcb_get_countries();
 	}
 
 }
