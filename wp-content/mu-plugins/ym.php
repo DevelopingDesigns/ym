420e82df7a696fee1f0590584cc759126f536957
Travis Smith <travis.smith@Traviss-MBP.lan>
2017-01-08 16:11:14 -0500

    Add acf-functions.php

--- wp-content/mu-plugins/ym.php
+++ wp-content/mu-plugins/ym.php
@@ -71,6 +71,7 @@ require_once( 'advanced-custom-fields-pro/acf.php' );
 
 // Templates
 require_once( 'ym-core/template-functions.php' );
+require_once( 'ym-core/acf-functions.php' );
 
 // Load ACF Builder
 require_once( 'wps-core/acf/acf-builder/lib/autoload.php' );
@@ -82,112 +83,4 @@ function ym_core_plugins_loaded() {
 	$fields = YM_Core_Fields::get_instance();
 }
 
-function ym_core_get_wrapper( $width = '', $class = '', $id = '' ) {
-	return array(
-		'width' => $width,
-		'class' => $class,
-		'id'    => $id,
-	);
-}
-
-/**
- * Change the default colors.
- *
- * @param array $colors Default of colors.
- *
- * @return array Array of colors.
- */
-function ym_core_get_theme_colors() {
-	return array(
-		array( 'btn-green' => __( 'Green', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-teal' => __( 'Teal', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-slate' => __( 'Slate', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-sec-green' => __( 'Secondary Green', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-sec-teal' => __( 'Secondary Teal', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-sec-slate' => __( 'Secondary Slate', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-orange' => __( 'Orange', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-red' => __( 'Red', YMCORE_PLUGIN_DOMAIN ), ),
-	);
-}
-
-/**
- * Change the default colors.
- *
- * @param array $colors Default of colors.
- *
- * @return array Array of colors.
- */
-function ym_core_get_bg_colors() {
-	return array(
-		array( 'none' => __( 'None', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'theme' => __( 'Theme', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'custom' => __( 'Colorpicker', YMCORE_PLUGIN_DOMAIN ), ),
-	);
-}
-
-/**
- * Change the default sizes.
- *
- * @param array $sizes Default of sizes.
- *
- * @return array Array of sizes.
- */
-function ym_core_get_sizes() {
-	return array(
-		array( 'btn-tiny' => __( 'Tiny', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-small' => __( 'Small', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-primary' => __( 'Default', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'btn-large' => __( 'Large', YMCORE_PLUGIN_DOMAIN ), ),
-	);
-}
-
-
-/**
- * Change the default sizes.
- *
- * @param array $sizes Default of sizes.
- *
- * @return array Array of sizes.
- */
-function ym_core_get_alignment() {
-	return array(
-		array( 'alignleft' => __( 'Left', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'aligncenter' => __( 'Center', YMCORE_PLUGIN_DOMAIN ), ),
-		array( 'alignright' => __( 'Right', YMCORE_PLUGIN_DOMAIN ), ),
-	);
-}
-
-/**
- * Template wrapper
- *
- * @param string $slug The slug name for the generic template.
- * @param string $load The name of the specialised template.
- */
-function ym_template( $slug, $name = null, $load = true ) {
-	static $ym_template;
-
-	if ( ! $ym_template ) {
-		$ym_template = new WPS_Template_Loader( array(
-			'filter_prefix'            => 'ym',
-			'theme_template_directory' => 'templates',
-			'templates_directory'      => 'templates',
-			'plugin_directory'         => YMCORE_PLUGIN_DIR,
-		) );
-	}
-
-	if ( defined( 'YMCORE_DEBUG' ) && YMCORE_DEBUG ) {
-		wps_printr(
-			array( 'template-path' => $ym_template->get_template_part( $slug, $name, false ) )
-		);
-	}
-
-//	wps_printr(array(
-//		'$templates' =>  $templates,
-//		'$slug' =>  $slug,
-//		'$load' =>  $load,
-//	));
-
-	return $ym_template->get_template_part( $slug, $name, $load );
-
-}
 
