fdbdde3a2b41ac36491604a3ef1a71c048ffbcd2
Travis Smith <travis.smith@Traviss-MBP.lan>
2017-01-07 21:05:53 -0500

    Add templates

---
+++ wp-content/mu-plugins/templates/contentblocks.php
@@ -0,0 +1,31 @@
+<?php
+// check if the flexible content field has rows of data
+if ( have_rows( 'blocks' ) ):
+
+//	if ( defined( 'YMCORE_DEBUG' ) && YMCORE_DEBUG ) {
+//		echo '<hr/>';
+//		echo 'Content Blocks ';
+//		echo '<hr/>';
+//
+//	}
+
+	// loop through the rows of data
+	while ( have_rows( 'blocks' ) ) : the_row();
+
+//		if ( defined( 'YMCORE_DEBUG' ) && YMCORE_DEBUG ) {
+//			echo '<hr/>';
+//			echo 'ROW # '; the_row_index();
+//			echo '<br/>';
+//			echo 'Row Layout: ';
+//			echo get_row_layout();
+//			echo '<hr/>';
+//		}
+
+		do_action( 'ym_before_block' );
+		ym_template( 'partials/' . get_row_layout(), true );
+		do_action( 'ym_after_block' );
+
+	endwhile;
+
+endif;
+
---
+++ wp-content/mu-plugins/templates/partials/featured_content.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'featured content template';
+echo '<br/>';
