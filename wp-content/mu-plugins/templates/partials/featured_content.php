0126b081cf5da4a09a85947b9335489267e98c6c
Travis Smith <travis.smith@Traviss-MacBook-Pro.local>
2017-01-09 18:39:35 -0500

    Add blank featured content template

--- wp-content/mu-plugins/templates/partials/featured_content.php
+++ wp-content/mu-plugins/templates/partials/featured_content.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Default code for a Featured Content Block partial
+ *
+ * @package    YourMembership
+ * @author     Developing Designs - Joe Dooley
+ * @link       https://www.developingdesigns.com
+ * @copyright  Joe Dooley, Developing Designs
+ * @license    GPL-2.0+
+ */
 
+ym_do_section_open( 'featured-content' );
 echo 'featured content template';
 echo '<br/>';
+ym_do_section_close();
