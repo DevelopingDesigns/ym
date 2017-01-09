d44c0a93fa4495f7efc1cd56f37b1892565091a5
Travis Smith <travis.smith@Traviss-MacBook-Pro.local>
2017-01-09 18:39:46 -0500

    Add blank resource library template

--- wp-content/mu-plugins/templates/partials/resource_library.php
+++ wp-content/mu-plugins/templates/partials/resource_library.php
@@ -1,3 +1,16 @@
 <?php
+/**
+ * Default code for a Logins Content Block partial
+ *
+ * @package    YourMembership
+ * @author     Developing Designs - Joe Dooley
+ * @link       https://www.developingdesigns.com
+ * @copyright  Joe Dooley, Developing Designs
+ * @license    GPL-2.0+
+ */
+
+ym_do_section_open( 'resource-library' );
 
 echo 'resource template';
+
+ym_do_section_close();
