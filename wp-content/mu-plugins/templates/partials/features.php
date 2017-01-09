95be96cd703081d951980efdefc31ae56f7fbb68
Travis Smith <travis.smith@Traviss-MacBook-Pro.local>
2017-01-09 18:38:41 -0500

    Add posts templates

--- wp-content/mu-plugins/templates/partials/features.php
+++ wp-content/mu-plugins/templates/partials/features.php
@@ -1,3 +1,12 @@
 <?php
+/**
+ * Default code for a Features Content Block partial
+ *
+ * @package    YourMembership
+ * @author     Developing Designs - Joe Dooley
+ * @link       https://www.developingdesigns.com
+ * @copyright  Joe Dooley, Developing Designs
+ * @license    GPL-2.0+
+ */
 
-echo 'features template';
+ym_posts( 'feature' );
