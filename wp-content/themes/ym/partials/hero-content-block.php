82c04da814d72952a967c15da0f8d01a706869c6
Joe Dooley <hello@developingdesigns.com>
2017-01-10 14:10:04 -0500

    added text links to call to action module

--- wp-content/themes/ym/partials/hero-content-block.php
+++ wp-content/themes/ym/partials/hero-content-block.php
@@ -9,29 +9,26 @@
  * @license    GPL-2.0+
  */
 
-
 $hero   = get_row( 'hero' );
 
-echo '<pre>';
-print_r( $hero );
-echo '</pre>';
-
 wp_localize_script( 'all-js', 'BackStretchImg', [
-	'src' => $hero_image['hero_image']['url']
+	'src' => $hero['hero_image']['url']
 ] );
 
 ?>
 
-<section class="<?php echo $hero['alignment']; ?> hero" style="background-image: url( <?php echo $hero['hero_image']['url'] ?> ); background-repeat: no-repeat;">
+<section class="hero <?php echo $hero['css_class']; ?>"
+         style="background-image: url( <?php echo $hero['hero_image']['url'] ?> ); justify-content: <?php echo $hero['alignment']; ?>;">
+	<div class="wrap">
 
-	<div class="hero-content">
-		<h1><?php echo $hero['heading']; ?></h1>
-		<h2><?php echo $hero['sub_heading']; ?></h2>
+		<div class="hero-content" style="text-align: <?php echo $hero['text_alignment']; ?>;">
+			<h1><?php echo $hero['heading']; ?></h1>
+			<h3><?php echo $hero['sub_heading']; ?></h3>
 
-		<?php if ( $hero['buttons'] ) :
-			get_template_part( 'partials/parts/button', 'group' );
-		endif; ?>
+			<?php if ( $hero['buttons'] ) :
+				get_template_part( 'partials/parts/button', 'group' );
+			endif; ?>
+		</div>
+
 	</div>
-
 </section>
-
