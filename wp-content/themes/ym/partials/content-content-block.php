3e75c3ad96a6b4202de4ccbc8482a5b5c189d961
Joe Dooley <hello@developingdesigns.com>
2017-01-10 19:43:59 -0500

    finished content-content-block. created scss parts dir and added button-group

--- wp-content/themes/ym/partials/content-content-block.php
+++ wp-content/themes/ym/partials/content-content-block.php
@@ -9,13 +9,24 @@
  * @license    GPL-2.0+
  */
 
+$content = get_row( 'content' );
+
+//echo '<pre>';
+//print_r( $content );
+//echo '</pre>';
 
 ?>
 
-<section class = "row  <?php the_sub_field( 'css_class' ); ?>">
+<section class = "row fc-content <?php echo $content['css_class']; ?>">
 	<div class = "wrap">
 		<div class = "content-container">
-			<?php the_sub_field( 'content' ); ?>
+			<h1><?php echo $content['heading']; ?></h1>
+			<p><?php echo $content['content_area']; ?></p>
+
+			<?php if ( $content['buttons'] ) :
+				get_template_part( 'partials/parts/button', 'group' );
+			endif; ?>
+
 		</div>
 	</div>
 </section>
