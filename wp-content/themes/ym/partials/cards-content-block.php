a32b57ac4ad4cdb7731d130ce1a80bbe2aaeeaab
Joe Dooley <hello@developingdesigns.com>
2017-01-22 10:43:15 -0500

    finished content image block

--- wp-content/themes/ym/partials/cards-content-block.php
+++ wp-content/themes/ym/partials/cards-content-block.php
@@ -9,53 +9,60 @@
  * @license    GPL-2.0+
  */
 
-
-// @todo Need to add a counter to determine css for the number of cards in a row.
-// @todo will be style = lost-column: 1/$count; Thats added to the card container for resuse.
-
-$cards = get_row('cards');
+$cards = get_row( 'cards' );
 
 //echo '<pre>';
 //print_r( $cards );
 //echo '</pre>';
 
-if (have_rows('card')) : ?>
+?>
 
-	<section class="row cards <?php echo $cards['css_class']; ?>"
-	         style="background-color: <?php echo $cards['background_color']; ?>;">
-		<div class="wrap">
-			<div class="flex-wrap">
+<section class="row cards <?php echo $cards['css_class']; ?>"
+         style="background-color: <?php echo $cards['background_color'] ?>;">
+	<div class="wrap">
 
-				<?php while (have_rows('card')) : the_row();
+		<?php if ( $cards['add_heading'] ) :
+			get_template_part( 'partials/parts/title', 'group' );
+		endif; ?>
 
-					$icon = get_sub_field('icon');
-					$heading = get_sub_field('heading');
-					$description = get_sub_field('description');
-					$add_cta = get_sub_field('add_cta');
 
-					?>
+		<?php if ( have_rows( 'card' ) ) : ?>
 
-					<article class="card-container">
-						<main>
-							<figure>
-								<img class="card" src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>">
-							</figure>
-							<div class="content-wrap">
-								<p class="heading"><?php echo $heading; ?></p>
-								<p class="description"><?php echo $description; ?></p>
-								<?php if ($add_cta) :
-									get_template_part('partials/parts/button', 'group');
-								endif; ?>
-							</div>
-						</main>
-					</article>
+		<div class="flex-wrap">
 
-				<?php endwhile; ?>
+		<?php while (have_rows('card')) : the_row();
 
-			</div>
+			$icon = get_sub_field('icon');
+			$heading = get_sub_field('heading');
+			$description = get_sub_field('description');
+			$add_cta = get_sub_field('add_cta');
+
+			?>
+
+				<article class="card-container">
+					<main>
+						<figure>
+							<img class="card" src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>">
+						</figure>
+						<div class="content-wrap">
+							<p class="heading"><?php echo $heading; ?></p>
+							<p class="description"><?php echo $description; ?></p>
+							<?php if ($add_cta) {
+								get_template_part( 'partials/parts/button', 'group' );
+							} ?>
+						</div>
+					</main>
+				</article>
+
+			<?php endwhile; ?>
+
 		</div>
-	</section>
 
-<?php endif; ?>
+		<?php endif; ?>
+
+	</div>
+
+</section>
+
 
 
--- wp-content/themes/ym/partials/content-image-content-block.php
+++ wp-content/themes/ym/partials/content-image-content-block.php
@@ -6,20 +6,21 @@
  * @author     Developing Designs - Joe Dooley
  * @link       https://www.developingdesigns.com
  */
+
 $content_img = get_row( 'content_and_image' );
+$img_url = $content_img['background_image']['url'];
+$bg_img = ! $content_img['background_image']['url'] ? 'background-image: none ;' :  'background-image: url(' . $img_url . ');';
+$bg_color = ! $content_img['add_bg'] ? 'background-color: #fff;' : 'background-color: ' . $content_img['background_color'] . ';';
+$css = $bg_img . $bg_color;
+
 
 //echo '<pre>';
 //print_r( $content_img );
 //echo '</pre>';
 
+?>
 
-if (  false !== $content_img['add_bg'] ) : ?>
-	<section class="row fc-content-image <?php echo $content_img['css_class']; ?>"
-         style="background-image: url(<?php echo $content_img['background_image']['url']; ?>);
-		         background-color:  <?php echo $content_img['background_color']; ?>;">
-		<?php endif; ?>
-
-		<section class="row fc-content-image <?php echo $content_img['css_class']; ?>">
+<section class="row fc-content-image <?php echo $content_img['css_class']; ?>" style="<?php echo $css ?>">
 
 	<div class="wrap">
 
@@ -31,7 +32,7 @@ if (  false !== $content_img['add_bg'] ) : ?>
 			<article>
 
 				<div class="content">
-					<div><?php echo $content_img['content_area']; ?></div>
+					<?php echo $content_img['content_area']; ?>
 
 					<?php if ( $content_img['add_cta'] ) :
 						get_template_part( 'partials/parts/button', 'group' );
@@ -40,13 +41,10 @@ if (  false !== $content_img['add_bg'] ) : ?>
 			</article>
 
 			<aside>
-				<div class="image-wrap" style="transform: translate( <?php echo $content_img['image_alignment'] ?> );">
-					<img src="<?php echo $content_img['image']['url'] ?>"
+				<img src="<?php echo $content_img['image']['url'] ?>"
 					     alt="<?php echo $content_img['image']['alt'] ?>"
 					     width="<?php echo $content_img['image']['alt'] ?>"
 					     height="<?php echo $content_img['image']['alt'] ?>">
-
-				</div>
 			</aside>
 
 		</main>
