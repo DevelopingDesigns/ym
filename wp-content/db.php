442710e0fb5fa208a3b5ffccd630a85ef8cfe2c4
Joe Dooley <hello@developingdesigns.com>
2017-06-28 05:34:06 -0400

    Installed Relevanssi to improve the default WP_Search. This way we can search custom fields on the blog in our Facets. Added caching to FacetWP. Added symlink for Query Monitor

--- wp-content/db.php
+++ wp-content/db.php
@@ -1,1 +1,30 @@
-/code/wp-content/plugins/query-monitor/wp-content/db.php
+<?php
+/**
+ * WordPress only allows a single db.php. Lets use Windows friendly
+ * symlinks so we can utilize more than one app that needs access to
+ * wp-content/db.php
+ */
+
+
+/**
+ * FacetWP Cache symlink
+ */
+if ( file_exists( WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php' ) ) {
+	include WP_CONTENT_DIR . '/plugins/facetwp-cache/cache.php';
+}
+
+
+/**
+ * WP Redis symlink
+ */
+if ( file_exists( WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php' ) ) {
+	require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
+}
+
+
+/**
+ * Query Monitor symlink
+ */
+if ( file_exists( WP_CONTENT_DIR . '/plugins/query-monitor/wp-content/db.php' ) ) {
+	require_once WP_CONTENT_DIR . '/plugins/query-monitor/wp-content/db.php';
+}
